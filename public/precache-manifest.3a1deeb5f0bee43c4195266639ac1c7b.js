a541cfc37f8bb0e5feebb85ae665b3507287af40
Pedro Tavares <xpedrotavaresdev@gmail.com>
2020-10-19 15:18:18 -0300

    set heroku

---
+++ public/precache-manifest.3a1deeb5f0bee43c4195266639ac1c7b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8aa2218aa6ab88a0599c9aff85ae3527",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2af04bc2a09b4adbcdb1",
+    "url": "/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "e865542517ad5cca1e08",
+    "url": "/static/css/main.655b15f8.chunk.css"
+  },
+  {
+    "revision": "2af04bc2a09b4adbcdb1",
+    "url": "/static/js/2.92e5ce24.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.92e5ce24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e865542517ad5cca1e08",
+    "url": "/static/js/main.2633755c.chunk.js"
+  },
+  {
+    "revision": "a128c93d5ed79c8b9071",
+    "url": "/static/js/runtime-main.f262d279.js"
+  },
+  {
+    "revision": "dd42df163b1037997f274c7a8100596f",
+    "url": "/static/media/capa-de-livro.dd42df16.jpg"
+  },
+  {
+    "revision": "9349a108f76bf40cfb5bfafb4d0a4f67",
+    "url": "/static/media/ceramica.9349a108.jpg"
+  },
+  {
+    "revision": "e0ec973befb74eabcc1805c2fa3039d7",
+    "url": "/static/media/logo.e0ec973b.png"
+  },
+  {
+    "revision": "ba5ba6eacf813e0bf9c0048d9a9da0ad",
+    "url": "/static/media/photo-card.ba5ba6ea.jpg"
+  },
+  {
+    "revision": "933baac71670ed87871f147186f7fee0",
+    "url": "/static/media/photo-chamada.933baac7.jpg"
+  },
+  {
+    "revision": "6f9efa56972dcb753d580cce9b84efe1",
+    "url": "/static/media/photo-manifesto.6f9efa56.jpg"
+  },
+  {
+    "revision": "c6bd543a46209763ef75b5fea55b57fb",
+    "url": "/static/media/tela.c6bd543a.jpg"
+  }
+]);
